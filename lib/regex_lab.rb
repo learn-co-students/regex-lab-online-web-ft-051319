b6ec6d1e160be1874d5a170fb0c085c0201eb94f
Haley DeKeyser <hcd09@live.com>
2019-05-16 03:18:11 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  word.match?(/\b[AEIOUaeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\b(un[a-z]{1,}ing)/).flatten
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-z]{5}\b/).flatten
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match?(/^[A-Z](\s|[a-zA-Z,'.?!]{1,}\s){1,}[a-zA-Z]{1,}[.?!]\z/)
 end
 
 def valid_phone_number?(phone)
-
+  phone.match?(/^\(?\d{3}\)?[- ]?\d{3}[- ]?\d{4}$/)
 end
